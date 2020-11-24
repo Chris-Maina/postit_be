db26eeccc9385fdfc49ef456c84e818ab2b22b90
Chris Maina <mainachrisw@gmail.com>
2020-11-24 13:30:46 +0300

    Add redis host and password env var

--- helpers/init_redis.js
+++ helpers/init_redis.js
@@ -1,7 +1,11 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
-const redisCient = redis.createClient(process.env.REDIS_PORT);
+const redisCient = redis.createClient([
+  host=port=process.env.REDIS_HOST,
+  port=process.env.REDIS_PORT,
+  password=process.env.REDIS_PASSWORD
+]);
 
 redisCient.on('connect', () => console.log('Connecting to redis...'));
 redisCient.on('ready', () => console.log('Connected to redis and ready to use...'));
