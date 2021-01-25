43565e53b6daf0a984f9324995dff76d5d66b086
Chris Maina <mainachrisw@gmail.com>
2021-01-25 16:51:56 +0300

    chore: check for broken connections and terminate
    - listen to websocket error and close events

--- index.js
+++ index.js
@@ -47,10 +47,33 @@ const PORT = process.env.PORT || 3100;
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server });
 webSocketServer.on("connection", (webSocket) => {
-  // console.info("Total connected clients:", webSocketServer.clients.size);
+  console.info("Total connected clients:", webSocketServer.clients.size);
+
+  webSocket.isAlive = true;
+  webSocket.on('pong', () => {
+    webSocket.isAlive = true;
+  });
 
   app.locals.wsInstance = webSocket
   app.locals.clients = webSocketServer.clients;
+});
+
+// Check broken connections and close
+const interval = setInterval(function ping() {
+  webSocketServer.clients.forEach(ws => {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(() => {});
+  }, 30000);
+});
+
+webSocketServer.on('error', (error) => {
+  console.info('websocket error', error)
+});
+
+webSocketServer.on('close', () => {
+  clearInterval(interval);
 });
 
 server.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
